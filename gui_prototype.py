fc9462508ede473624d1b2858be60c8cf9e074e1
AshtoniousMaximus <greenmtbkid@gmail.com>
2025-01-21 15:14:08 -0800

    fixed GUI interfaces
    I made the GUI not suck, because it actually works now. I added a slider, a checkbox, and a button with corresponding functions to interact with other files

--- gui_prototype.py
+++ gui_prototype.py
@@ -17,6 +17,24 @@ frames = [
 ]
 
 
+# a function that does nothing
+def functions():
+    """
+    this function does absolutely nothing
+
+    """
+    pass
+
+
+# a function that is supposed to run a simulation
+def run_sample():
+    """
+    same goes for this one
+
+    """
+    pass
+
+
 # taken and modified from stackoverflow
 # creates a function called update
 def update(ind):
@@ -42,6 +60,7 @@ def update(ind):
 # starts the animation
 window.after(0, update, 0)
 
+
 # aliases the word label with the Label function in tkinter
 label = Label(window)
 # packs the label function
@@ -49,12 +68,23 @@ label.pack()
 # sets the title of the window
 window.title("College sign simulator")
 
-# creates a label for an status message
-status_label = tk.Label(window)
-# packs the message
-status_label.pack()
+# creates a text label for a checkbox
+# randomized_slides_label = tk.Label(window, text="Randomized slide order")
+# randomized_slides_label.pack()
 
+var1 = bool
 
+# slider and checkbox were modified from codemy.com youtube channel
+randomized_slide_checkbox = tk.Checkbutton(window, text="Randomize slide order", variable=var1, onvalue=1, offvalue=0, command=functions)
+randomized_slide_checkbox.pack()
+
+speed_bump_slider = tk.Scale(window, from_=0, to=5, orient="horizontal")
+speed_bump_slider.pack()
+
+# creates a button to run the simulaton
+entry_button = tk.Button(window, text='press to start simulation', command=run_sample)
+# packs the entry button to the gui
+entry_button.pack()
 
 # runs the gui window in a loop
 window.mainloop()
